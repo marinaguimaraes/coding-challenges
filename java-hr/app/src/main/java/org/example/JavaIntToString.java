0cf2a6bf1d14ba6f93d39e0eac8d92f88cca17cb
Marina Guimaraes <guimaraesmarina98@gmail.com>
2025-03-16 14:41:06 +0100

    java Int to String

---
+++ java-hr/app/src/main/java/org/example/JavaIntToString.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.*;
+import java.security.*;
+
+public class JavaIntToString {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.close();
+
+        String s = String.valueOf(n);
+
+        if (n == Integer.parseInt(s)) {
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer.");
+        }
+    }
+}
