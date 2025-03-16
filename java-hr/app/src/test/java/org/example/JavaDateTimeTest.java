56392213c5adec7bea33f62208431298f9886ec7
Marina Guimaraes <guimaraesmarina98@gmail.com>
2025-03-16 16:25:08 +0100

    java date and time

---
+++ java-hr/app/src/test/java/org/example/JavaDateTimeTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class JavaDateTimeTest {
+
+    @Test
+    public void testInputSample() {
+        int month = 8;
+        int day = 14;
+        int year = 2017;
+
+        String dayOfWeek = JavaDateTime.Result.findDay(month,day,year);
+
+        assertEquals("MONDAY", dayOfWeek);
+    }
+
+    @Test
+    public void testSecondInputSample() {
+        int month = 8;
+        int day = 5;
+        int year = 2015;
+
+        String dayOfWeek = JavaDateTime.Result.findDay(month,day,year);
+
+        assertEquals("WEDNESDAY", dayOfWeek);
+    }
+}
