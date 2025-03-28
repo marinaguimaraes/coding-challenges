9416a541946443d3014a96fbd747f72a8d2bc601
Marina Guimaraes <guimaraesmarina98@gmail.com>
2025-03-28 18:22:46 +0100

    java big decimal

--- java-hr/app/src/main/java/prepare/java/bignumber/JavaBigDecimal.java
+++ java-hr/app/src/main/java/prepare/java/bignumber/JavaBigDecimal.java
@@ -1,4 +1,46 @@
 package prepare.java.bignumber;
 
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class JavaBigDecimal {
+
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+        sc.close();
+
+        sortArray(n, s);
+
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+
+    static void sortArray(int n, String[] s) {
+        for (int i = 0; i < n-1; i++) {
+            for (int j = i+1; j < n; j++) {
+                String strI = s[i];
+                String strJ = s[j];
+                BigDecimal bdI = new BigDecimal(strI);
+                BigDecimal bdJ = new BigDecimal(strJ);
+                if (bdI.compareTo(bdJ) < 0) {
+                    // shift values to keep order
+                    for (int k=j; k>i; k--) {
+                        s[k] = s[k-1];
+                    }
+                    s[i] = strJ;
+                }
+            }
+        }
+    }
+
 }
--- java-hr/app/src/test/java/prepare/java/bignumber/JavaBigDecimalTest.java
+++ java-hr/app/src/test/java/prepare/java/bignumber/JavaBigDecimalTest.java
@@ -1,4 +1,40 @@
 package prepare.java.bignumber;
 
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import static org.junit.Assert.assertEquals;
+
 public class JavaBigDecimalTest {
+
+    @Test
+    public void testSampleInput() {
+        int n = 9;
+        String[] s = new String[]{"-100", "50", "0", "56.6", "90", "0.12", ".12", "02.34", "000.000", null, null};
+
+        JavaBigDecimal.sortArray(n, s);
+
+        assertEquals("90", s[0]);
+        assertEquals("56.6", s[1]);
+        assertEquals("50", s[2]);
+    }
+
+    @Test
+    public void testInputMultipleEquivalentValues() {
+        int n = 7;
+        String[] s = new String[]{"-100", "-100.00", "0.00", "000.000", "-0100", "0.0", ".0", null, null};
+
+        JavaBigDecimal.sortArray(n, s);
+
+        assertEquals("0.00", s[0]);
+        assertEquals("000.000", s[1]);
+        assertEquals("0.0", s[2]);
+        assertEquals(".0", s[3]);
+        assertEquals("-100", s[4]);
+        assertEquals("-100.00", s[5]);
+        assertEquals("-0100", s[6]);
+    }
+
 }
